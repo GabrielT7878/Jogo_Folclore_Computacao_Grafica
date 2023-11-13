53613d8cdbb4089002ea51f4284cbadab6f838b8
Gabrie.T <gabrielfernandesthiago@hotmail.com>
2023-11-13 20:22:21 -0300

    attack implemented

--- Assets/Scripts/Character/ControlPlayer.cs
+++ Assets/Scripts/Character/ControlPlayer.cs
@@ -14,7 +14,6 @@ public class ControlPlayer : MonoBehaviour
     public bool isGrounded;
     public float groundCheckSize;
     public Vector3 groundCheckPosition;
-    public int rollForce;
     public bool roll = false;
     private int side = 1;
     public float runSpeed;
@@ -22,8 +21,9 @@ public class ControlPlayer : MonoBehaviour
     private float horizontal;
     public Camera cam;
     public PlayableDirector playableDirector;
-
-
+    public GameObject lanca;
+    public Transform maoDireita;
+    public GameObject ball;
     public Animator anim;
 
     void Start()
@@ -36,6 +36,16 @@ public class ControlPlayer : MonoBehaviour
 
         horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        if (vertical > 0.01f && rb.isKinematic)
+        {
+            rb.transform.Translate(new Vector3(0, 1, 0) * walkSpeed * Time.deltaTime);
+        }
+        else if (vertical < -0.01f && rb.isKinematic)
+        {
+            rb.transform.Translate(new Vector3(0, -1, 0) * walkSpeed * Time.deltaTime);
+        }
+
         if (!roll)
         {
             if (Input.GetButtonDown("Fire3") && !roll)
@@ -133,13 +143,22 @@ public class ControlPlayer : MonoBehaviour
             transform.Translate(new Vector3(0, 0, side) * walkSpeed * 0.5f * Time.deltaTime);
         }
 
-    }
+        if (horizontal != 0 && rb.isKinematic)
+        {
+            rb.isKinematic = false;
+            anim.SetBool("subir", false);
+        }
 
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            anim.SetTrigger("attack");
+        }
+
+    }
     public void endRoll(int i)
     {
         if (i == 1)
         {
-
             roll = false;
             col.height = 2.45f;
             col.center = new Vector3(0, 1.19f, 0);
@@ -154,11 +173,19 @@ public class ControlPlayer : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// OnCollisionEnter is called when this collider/rigidbody has begun
-    /// touching another rigidbody/collider.
-    /// </summary>
-    /// <param name="other">The Collision data associated with this collision.</param>
+    public void attack(){
+        GameObject l = Instantiate(lanca, maoDireita.position, Quaternion.identity);
+        l.transform.localScale = new Vector3(30f, 30f, 30f);
+        l.AddComponent<Rigidbody>();
+        l.GetComponent<Rigidbody>().useGravity = false;
+        l.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 10 * side);
+
+        //Rigidbody lb = l.GetComponent<Rigidbody>();
+        //lb.isKinematic = true;
+        //lb.velocity = new Vector3(0,0,2 * side);
+        //l.GetComponent<Rigidbody>().AddForce(transform.forward * 10, ForceMode.Impulse);
+    }
+
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Jumper")
@@ -189,6 +216,11 @@ public class ControlPlayer : MonoBehaviour
             cine.enabled = true;
             playableDirector.Play();
         }
+        
+        if (other.gameObject.tag == "ball")
+        {
+            ball.GetComponent<Rigidbody>().isKinematic = false;
+        }
     }
 
     void OnCollisionExit(Collision other)
@@ -200,8 +232,14 @@ public class ControlPlayer : MonoBehaviour
         }
     }
 
-
-
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Cipo")
+        {
+            rb.isKinematic = true;
+            anim.SetBool("subir", true);
+        }
+    }
 
     private void OnDrawGizmos()
     {
